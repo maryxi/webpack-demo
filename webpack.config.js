7229fbef1b868c97be59211b873f7ef4d2c42227
maxincheng <maxincheng@didichuxing.com>
2016-09-23 11:49:15 +0800

    modify config

--- webpack.config.js
+++ webpack.config.js
@@ -8,10 +8,17 @@ var APP_PATH = path.resolve(ROOT_PATH, 'app')
 var BUILD_PATH = path.resolve(ROOT_PATH, 'build')
 
 module.exports = {
-    // APP_PATH项目的文件夹名字，直接用文件夹的名字会默认的着index.js ，也可以直接指定文件的名字
-    // 
+    /* APP_PATH项目的文件夹名字，直接用文件夹的名字会默认的着index.js ，也可以直接指定文件的名字
+     * 分离app.js和第三方库
+     * run build 之后会生成 index.js和vendors.js
+     * 
+     */ 
     entry: {
         app: path.resolve(APP_PATH, 'index.js'),
+
+        /* 添加要打包在vendors里面的库
+         * vendors对应chunkName的参数，必须保持一样 否则不能正常的加载
+         */
         vendors: ['jquery'],
     },
     // output: {
@@ -26,6 +33,12 @@ module.exports = {
             $: 'jquery'
         })
     ],
+
+    /*
+     * 一下配置实现热加载
+     * 在package.json中的scripts对象里面添加 "dev": "webpack-dev-server --hot --inline"
+     * 
+     */
     devServer: {
         historyApiFallback: true,
         hot: true,
@@ -42,6 +55,11 @@ module.exports = {
 
     }, 
     module: {
+        /*
+         * 以下是loader的使用，执行的顺序是按照从右至左的顺序
+         * 也可以写成数组的形式 ['style', 'css', 'less']
+         *
+         */
         loaders: [
             {
                 test: /\.scss$/,
@@ -53,6 +71,10 @@ module.exports = {
                 loader: 'style!css',
                 include: APP_PATH
             },
+            /*
+             * url-loader
+             * 当图片小于25KB的时候，在它从属的css文件中转成BASE64字符串
+             */
             {
                 test: /\.(jpg|png)$/,
                 loader: 'url?limit=40000',
@@ -62,11 +84,18 @@ module.exports = {
                 test: /\.js$/,
                 loader: 'babel',
                 include: APP_PATH,
+
+                // es2015这个参数是babel的plugin 支持各种es6新特性
                 query: {
                     presets: ['es2015']
                 }
             }
         ],
+
+        /*
+         * js的语法检查
+         * perLoaders 就是在loaders之前执行
+         */
         perLoaders: [
             {
                 test: /\.js$/,
@@ -75,9 +104,13 @@ module.exports = {
             }
         ]
     },
+
+    //配置jshint的选项，支持es6的校验
     jshint: {
         'esnext': true
     },
+
+    // 出错以后会以source-map的形式直接显示你出错的代码位置
     devtool: 'eval-source-map'
 }
 
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -3,29 +3,43 @@ var webpack = require('webpack')
 var HtmlwebpackPlugin = require('html-webpack-plugin')
 
 // 文件夹的路径
+// __dirname表示当前目录
+// 
 var ROOT_PATH = path.resolve(__dirname)
 var APP_PATH = path.resolve(ROOT_PATH, 'app')
 var BUILD_PATH = path.resolve(ROOT_PATH, 'build')
 var TEM_PATH = path.resolve(ROOT_PATH, 'templates')
 
+//编译输出路径
 module.exports = {
     // APP_PATH项目的文件夹名字，直接用文件夹的名字会默认的着index.js ，也可以直接指定文件的名字
-    // 
     entry: {
         app: path.resolve(APP_PATH, 'index.js'),
         mobile: path.resolve(APP_PATH, 'mobile.js'),
         vendors: ['jquery']
     },
     output: {
+
+        // 发不到哪个目录下面
         path: BUILD_PATH,
+
+        // 合并后的js会命名为
+        // hash 加上他 防止缓存
         filename: '[name].[hash].js'
     },
+    // webpack的插件
     plugins: [
         // 使用Uglify.js压缩js代码
         new webpack.optimize.UglifyJsPlugin({minimize: true}),
         // 入口文件里面的数组打包成vendors.js
+        /*
+         * 提公用js到vendors.js文件中
+         * 
+         */
+                                        /* chunkName= */ /* filename= */
         new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
         
+        // 添加插件 自动生成一个html
         new HtmlwebpackPlugin({
             title: 'hello World mxc',
             template: path.resolve(TEM_PATH, 'index.html'),
@@ -44,6 +58,8 @@ module.exports = {
             // 把script 插入到标签里面
             inject: 'body'
         }),
+
+        // 使用 ProvidePlugin 加载使用率高的依赖库
         new webpack.ProvidePlugin({
             $: 'jquery'
         })
